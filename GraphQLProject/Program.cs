f4e001cda2f01d79ef5ba6174bf1609dfa31c9a6
User <yarik.sawchuck3102004@gmail.com>
2025-01-14 16:00:59 +0200

    Building a GraphQL API with .NET Core

--- GraphQLProject/Program.cs
+++ GraphQLProject/Program.cs
@@ -1,14 +1,36 @@
+using GraphiQl;
+using GraphQL;
+using GraphQL.Types;
+using GraphQLProject.Interfaces;
+using GraphQLProject.Mutation;
+using GraphQLProject.Query;
+using GraphQLProject.Schema;
+using GraphQLProject.Services;
+using GraphQLProject.Type;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddTransient<IMenuRepository, MenuRepository>();
+builder.Services.AddTransient<MenuType>();
+builder.Services.AddTransient<MenuQuery>();
+builder.Services.AddTransient<MenuMutation>();
+builder.Services.AddTransient<MenuInputType>();
+
+builder.Services.AddTransient<ISchema,MenuSchema>();
+
+builder.Services.AddGraphQL(b => b.AddAutoSchema<ISchema>().AddSystemTextJson());
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseGraphiQl("/graphql");
+app.UseGraphQL<ISchema>();
 
 app.UseAuthorization();
 
